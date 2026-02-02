56e199f957e7cf88c0b76483e1a3a2e577a1109a
jjosephperez <jjperez8@uci.edu>
2026-02-02 15:23:08 -0800

    Update roundWinner.m

--- roundWinner.m
+++ roundWinner.m
@@ -1,8 +1,8 @@
 function result = roundWinner(playerScore, dealerScore)
 % determines round winner based on respective hand scores
-    if (playerScore > 21)
+    if (playerScore > 21) & (dealerScore <= 21)
         result = "Player Busts. Dealer wins.";
-    elseif (dealerScore > 21)
+    elseif (dealerScore > 21) & (playerScore <= 21)
         result = "Dealer Busts. Player wins";
     elseif (playerScore > dealerScore)
         result = "Player Wins.";
